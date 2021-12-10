8e540f1e8ba6685f42ab22b15b0438224871207c
Daniel MS <danielmccannsayles@gmail.com>
2021-12-10 00:23:31 -0800

    added main page and individual pages
    finished tutorial: https://www.sanity.io/guides/build-your-first-blog-using-react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllPosts from "./components/AllPosts.js";
 import OnePost from "./components/OnePost.js";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Route component={AllPosts} path="/" exact />
-        <Route component={OnePost} path="/:slug" />
-      </div>
+      <Routes>
+        <Route path="/" element={<AllPosts/>} />
+        <Route path="/:slug" element={<OnePost/>} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/components/OnePost.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { useParams, Link} from "react-router-dom";
+import sanityClient from "../client.js";
+import BlockContent from "@sanity/block-content-to-react";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
+
+export default function OnePost() {
+  const [postData, setPostData] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == $slug]{
+          title,
+          slug,
+          mainImage{
+            asset->{
+              _id,
+              url
+             }
+           },
+         body,
+        "name": author->name,
+        "authorImage": author->image
+       }`,
+        { slug }
+      )
+      .then((data) => setPostData(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!postData) return <div>Loading...</div>;
+
+  return (
+    <div>
+    <Link to={"/"} style={{ textDecoration: 'none' }} >
+    <div>Click to go back</div>
+    </Link>
+      <div>
+        <h2>{postData.title}</h2>
+        <div>
+          {postData.authorImage && <img
+            src={urlFor(postData.authorImage).width(100).url()}
+            alt="Author is Kap"
+          />}
+          <h4>{postData.name}</h4>
+        </div>
+      </div>
+      {postData.mainImage && <img src={urlFor(postData.mainImage).width(200).url()} alt="" />}
+      <div>
+        <BlockContent
+          blocks={postData.body}
+          projectId={sanityClient.projectId}
+          dataset={sanityClient.dataset}
+        />
+      </div>
+    </div>
+  );
+}
